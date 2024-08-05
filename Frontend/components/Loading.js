6e98f977fa6b200dee8682f774f5547c3fbaf212
muktar <muktarabdela10@gmail.com>
2024-08-05 23:27:48 +0300

    secure some folders , add loading and more

--- Frontend/components/Loading.js
+++ Frontend/components/Loading.js
@@ -5,7 +5,7 @@ const Loading = () => {
     return (
         <div className="flex flex-col justify-center items-center space-y-4">
             <Loader className="animate-spin text-primary" size={30} />
-            <p className="text-lg font-medium text-primary">Loading...</p>
+            <p className="text-lg font-medium text-primary"></p>
         </div>
     );
 };
